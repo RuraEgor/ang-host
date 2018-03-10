483babb7fc299046cc0be95773f4cdb044d2d6b6
Rura Egor <ruraegor@gmail.com>
2018-03-10 22:40:14 +0300

    first commit

---
+++ src/app/modules/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { MainPageComponent } from '../../component/main-page/main-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
